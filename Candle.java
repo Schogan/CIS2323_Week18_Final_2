6fd9bd2776a1d7f53b2f240f992c051ad02681a2
Schogan <navy15@gmail.com>
2018-12-12 18:33:03 -0600

    update

---
+++ Candle.java
@@ -0,0 +1,24 @@
+
+public class Candle{
+	
+	String color = "placeholder";
+	int height = 0;
+	double price = 0;
+	
+	public static void setColor(String color){
+		
+	}
+	public static void setHeight(int height){
+		
+		
+	}
+	public double getPrice(){
+		
+	}
+	public String getColor(){
+		return color;
+	}
+	public int getHeight(){
+		return height;
+	}
+}
